b8ff9a391f43fe40e22f631583cd0ae40e75e919
ehsanfathi-sys <ehsan.fathi@foxprimary.co.uk>
2026-07-27 14:51:17 +0100

    Remove aura feature file and complete rollback

--- piano-magic.js
+++
@@ -1,6 +0,0 @@
-(function(){
-'use strict';
-// Temporarily disabled: the first visual-effects implementation caused
-// excessive DOM work on some devices and made live note display glitchy.
-// Keep this file as a safe no-op so cached index.html versions remain stable.
-}());
